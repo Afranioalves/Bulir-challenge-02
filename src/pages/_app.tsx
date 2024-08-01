8810922857c8e80167bbfdf87dd91ca861e8c36e
Afranioalves <afranio777alves@gmail.com>
2024-08-02 00:18:22 +0100

    style: importar estilos

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import "@/styles/globals.css";
 import "@/styles/boxicons-2.1.4/css/boxicons.css"
 import "@/styles/aos-css/aos.css";
+import 'swiper/css';
+import 'swiper/css/pagination';
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
