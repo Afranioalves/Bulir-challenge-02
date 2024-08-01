224bbb847bbe25cc30ff1488b91f7b351a89aa68
Afranioalves <afranio777alves@gmail.com>
2024-08-01 17:35:46 +0100

    chore: importar components e estilos

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import "@/styles/globals.css";
+import "@/styles/boxicons-2.1.4/css/boxicons.css"
+import "@/styles/aos-css/aos.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,6 +2,8 @@ import styles from './style.module.css'
 import Metadata from '@/components/metadata'
 import Heading from '@/components/heading'
 import Category from '@/components/category'
+import Service from '@/components/service'
+import Provider from '@/components/provider'
 
 const Home = () =>{
     return(
@@ -10,6 +12,8 @@ const Home = () =>{
              <main>
                <Heading />
                <Category />
+               <Service />
+               <Provider />
             </main>
         </>
        
